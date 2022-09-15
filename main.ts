d180ef8c3dc21d2b8102d9e6fd6ac3e6d637659b
XGGMasterX <gianfrancoenrique95@gmail.com>
2022-09-15 01:50:54 -0300

    Guardado de pruebas

---
+++ main.ts
@@ -0,0 +1,11 @@
+const pepe = "10";
+console.log(pepe);
+const pepe2 = "100";
+console.log(pepe2);
+
+let dupla :[string,number,boolean] = [pepe,10,true];
+console.log(dupla);
+
+dupla[0] = "hola";
+dupla[1] = 100,true;
+console.log(dupla);
